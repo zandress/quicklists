783257818debe3e3978fd1547e2bfb31d5c9dcfe
Zach <zandress1@gmail.com>
2022-12-14 15:13:20 -0700

    Creating project and setup
    Removing unneeded files and creating new ones in its place. Setting up default routing and structure.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import {
+  PreloadAllModules,
+  RouteReuseStrategy,
+  RouterModule,
+} from '@angular/router';
+
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: 'app.component.html',
-  styleUrls: ['app.component.scss'],
+    selector: 'app-root',
+    template: `
+        <ion-app>
+            <ion-router-outlet></ion-router-outlet>
+        </ion-app>
+    `,
+    changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AppComponent {
-  constructor() {}
-}
+export class AppComponent {}
+
+@NgModule({
+    declarations: [AppComponent],
+    entryComponents: [],
+    imports: [
+        BrowserModule,
+        IonicModule.forRoot(),
+        RouterModule.forRoot(
+            [
+                {
+                    path: 'home',
+                    loadChildren: () =>
+                        import('./home/home.component').then((m) => m.HomeComponentModule),
+                },
+                {
+                    path: '',
+                    redirectTo: 'home',
+                    pathMatch: 'full',
+                },
+            ],
+            { preloadingStrategy: PreloadAllModules }
+        ),
+    ],
+    providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+    bootstrap: [AppComponent],
+})
+export class AppModule {}
