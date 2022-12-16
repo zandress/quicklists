2638cacbaae8f8ca07853b21c4cd036a3dcb4b62
zandress <zandress1@gmail.com>
2022-12-16 14:31:00 -0700

    setting up the checklist.component in the root module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,39 +9,46 @@ import {
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 @Component({
-    selector: 'app-root',
-    template: `
-        <ion-app>
-            <ion-router-outlet></ion-router-outlet>
-        </ion-app>
-    `,
-    changeDetection: ChangeDetectionStrategy.OnPush,
+  selector: 'app-root',
+  template: `
+    <ion-app>
+      <ion-router-outlet></ion-router-outlet>
+    </ion-app>
+  `,
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {}
 
 @NgModule({
-    declarations: [AppComponent],
-    entryComponents: [],
-    imports: [
-        BrowserModule,
-        IonicModule.forRoot(),
-        RouterModule.forRoot(
-            [
-                {
-                    path: 'home',
-                    loadChildren: () =>
-                        import('./home/home.component').then((m) => m.HomeComponentModule),
-                },
-                {
-                    path: '',
-                    redirectTo: 'home',
-                    pathMatch: 'full',
-                },
-            ],
-            { preloadingStrategy: PreloadAllModules }
-        ),
-    ],
-    providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
-    bootstrap: [AppComponent],
+  declarations: [AppComponent],
+  entryComponents: [],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    RouterModule.forRoot(
+      [
+        {
+          path: 'home',
+          loadChildren: () =>
+            import('./home/home.component').then((m) => m.HomeComponentModule),
+        },
+        {
+          path: 'checklist/:id',
+          loadChildren: () =>
+            import('./checklist/checklist.component').then(
+              (m) => m.ChecklistComponentModule
+            ),
+        },
+        {
+          path: '',
+          redirectTo: 'home',
+          pathMatch: 'full',
+        },
+      ],
+      { preloadingStrategy: PreloadAllModules }
+    ),
+  ],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
