8f06669d17999a1b9e19a635186152effc66d805
zandress <zandress1@gmail.com>
2022-12-16 12:24:09 -0700

    created and wrote the Checklist service

---
+++ src/app/shared/data-access/checklist.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from "@angular/core";
+import { BehaviorSubject } from "rxjs";
+import { Checklist } from "../interfaces/checklist";
+
+
+@Injectable
+({
+  providedIn: 'root',
+})
+export class ChecklistService {
+  private checklists$ = new BehaviorSubject<Checklist[]>([]);
+
+  getChecklists() {
+    return this.checklists$.asObservable()
+  }
+
+  add(checklist: Pick<Checklist, 'title'>) {
+    const newChecklist = {
+      ...checklist,
+      id: this.generateSlug(checklist.title),
+    };
+
+    this.checklists$.next([...this.checklists$.value, newChecklist]);
+  }
+
+  private generateSlug(title: string) {
+    // NOTE: This is a simplistic slug generator and will not handle things like special characters.
+    let slug = title.toLowerCase().replace(/\s+/g, '-');
+
+    // Check if the slug already exists
+    const matchingSlugs = this.checklists$.value.find(
+      (checklist) => checklist.id === slug
+    );
+
+    // If the title is already being used, add a string to make the slug unique
+    if (matchingSlugs) {
+      slug = slug + Date.now().toString();
+    }
+
+    return slug;
+  }
+}
