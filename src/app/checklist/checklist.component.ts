475f9bf5920c4300e70f317a27a4f44b7028a087
zandress <zandress1@gmail.com>
2022-12-19 14:09:53 -0700

    creating a generic form modal component

--- src/app/checklist/checklist.component.ts
+++ src/app/checklist/checklist.component.ts
@@ -1,7 +1,9 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, NgModule } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 import { ActivatedRoute, Router, RouterModule } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
+import { BehaviorSubject } from 'rxjs';
 import { switchMap } from 'rxjs/operators';
 import { __param } from 'tslib';
 import { ChecklistService } from '../shared/data-access/checklist.service';
@@ -25,6 +27,12 @@ import { ChecklistService } from '../shared/data-access/checklist.service';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ChecklistComponent {
+  formModalIsOpen$ = new BehaviorSubject<boolean>(false);
+
+  checklistItemForm = this.fb.nonNullable.group({
+    title: ['', Validators.required],
+  });
+
   checklist$ = this.route.paramMap.pipe(
     switchMap((paramMap) =>
       this.checklistService.getChecklistById(paramMap.get('id') as string)
@@ -33,7 +41,8 @@ export class ChecklistComponent {
 
   constructor(
     private route: ActivatedRoute,
-    private checklistService: ChecklistService
+    private checklistService: ChecklistService,
+    private fb: FormBuilder,
   ) {}
 }
 
