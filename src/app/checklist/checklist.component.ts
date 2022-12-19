2e453504b488c27e7fe8ad470bf3e5353681efd2
zandress <zandress1@gmail.com>
2022-12-19 15:35:42 -0700

    added modal to checklistcomponent

--- src/app/checklist/checklist.component.ts
+++ src/app/checklist/checklist.component.ts
@@ -7,6 +7,7 @@ import { BehaviorSubject } from 'rxjs';
 import { switchMap } from 'rxjs/operators';
 import { __param } from 'tslib';
 import { ChecklistService } from '../shared/data-access/checklist.service';
+import { FormModalComponentModule } from "../shared/ui/form-modal.component";
 
 @Component({
   selector: 'app-checklist',
@@ -28,7 +29,21 @@ import { ChecklistService } from '../shared/data-access/checklist.service';
         </ion-toolbar>
       </ion-header>
 
-      <ion-content> </ion-content>
+      <ion-content>
+        <ion-modal
+          [isOpen]="vm.formModalIsOpen"
+          [canDismiss]="true"
+          (ionModalDidDismiss)="formModalIsOpen$.next(false)"
+        >
+          <ng-template>
+            <app-form-modal
+              title="Create item"
+              [formGroup]="checklistItemForm"
+              (save)="addChecklistItem(vm.checklist.id)"
+            ></app-form-modal>
+          </ng-template>
+        </ion-modal>
+      </ion-content>
     </ng-container>
   `,
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -54,16 +69,17 @@ export class ChecklistComponent {
 }
 
 @NgModule({
-  imports: [
-    CommonModule,
-    IonicModule,
-    RouterModule.forChild([
-      {
-        path: '',
-        component: ChecklistComponent,
-      },
-    ]),
-  ],
-  declarations: [ChecklistComponent],
+    declarations: [ChecklistComponent],
+    imports: [
+        CommonModule,
+        IonicModule,
+        RouterModule.forChild([
+            {
+                path: '',
+                component: ChecklistComponent,
+            },
+        ]),
+        FormModalComponentModule
+    ]
 })
 export class ChecklistComponentModule {}
