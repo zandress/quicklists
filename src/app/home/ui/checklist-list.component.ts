1534c8b07ffa730d182839a6bde961074ef1515f
zandress <zandress1@gmail.com>
2022-12-20 16:00:33 -0700

    adding edit and delete buttons and outputs for the methods

--- src/app/home/ui/checklist-list.component.ts
+++ src/app/home/ui/checklist-list.component.ts
@@ -2,8 +2,10 @@ import { CommonModule } from '@angular/common';
 import {
   ChangeDetectionStrategy,
   Component,
+  EventEmitter,
   Input,
   NgModule,
+  Output,
 } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
@@ -13,20 +15,36 @@ import { Checklist } from 'src/app/shared/interfaces/checklist';
   selector: 'app-checklist-list',
   template: `
     <ion-list lines="none">
-      <ion-item
+      <ion-item-sliding
         *ngFor="let checklist of checklists; trackBy: trackByFn"
-        button
-        routerLink="/checklist/{{ checklist.id }}"
-        routerDirection="forward"
       >
-        <ion-label>{{ checklist.title }}</ion-label>
-      </ion-item>
+        <ion-item
+          *ngFor="let checklist of checklists; trackBy: trackByFn"
+          button
+          routerLink="/checklist/{{ checklist.id }}"
+          routerDirection="forward"
+        >
+          <ion-label>{{ checklist.title }}</ion-label>
+        </ion-item>
+
+        <ion-item-options side="end">
+          <ion-item-option color="light" (click)="edit.emit(checklist)">
+            <ion-icon name="pencil-outline" slot="icon-only"></ion-icon>
+          </ion-item-option>
+          <ion-item-option color="danger" (click)="delete.emit(checklist.id)">
+            <ion-icon name="trash" slot="icon-only"></ion-icon>
+          </ion-item-option>
+        </ion-item-options>
+      </ion-item-sliding>
     </ion-list>
   `,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ChecklistListComponent {
   @Input() checklists!: Checklist[];
+
+  @Output() delete = new EventEmitter<string>();
+  @Output() edit = new EventEmitter<Checklist>();
 
   constructor() {}
 
