561241741d24c7fcc62342be3a59fc2b28768147
Zach <74432623+zandress@users.noreply.github.com>
2022-12-27 11:44:37 -0700

    Revert "ui for editing checklists"
    This reverts commit 8ac7dcc2d5eb1287c3036062609b25b59660926e.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -5,7 +5,6 @@ import { Router, RouterModule } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
 import { BehaviorSubject } from 'rxjs';
 import { ChecklistService } from '../shared/data-access/checklist.service';
-import { Checklist } from '../shared/interfaces/checklist';
 import { FormModalComponentModule } from '../shared/ui/form-modal.component';
 import { ChecklistListComponentModule } from './ui/checklist-list.component';
 
@@ -27,30 +26,17 @@ import { ChecklistListComponentModule } from './ui/checklist-list.component';
         *ngIf="checklist$ | async as checklists"
         [checklists]="checklists"
         (delete)="deleteChecklist($event)"
-        (edit)="openEditModal($event)"
       ></app-checklist-list>
       <ion-modal
-        *ngIf="{
-          checklistIdBeingEdited: checklistIdBeingEdited$ | async,
-          isOpen: formModalIsOpen$ | async
-        } as vm"
-        [isOpen]="vm.isOpen"
+        [isOpen]="formModalIsOpen$ | async"
         [canDismiss]="true"
-        (ionModalDidDismiss)="
-          formModalIsOpen$.next(false); checklistIdBeingEdited$.next(null)
-        "
+        (ionModalDidDismiss)="formModalIsOpen$.next(false)"
       >
-      <ng-template>
+        <ng-template>
           <app-form-modal
-            [title]="
-              vm.checklistIdBeingEdited ? 'Edit checklist' : 'Create checklist'
-            "
+            title="Create checklist"
             [formGroup]="checklistForm"
-            (save)="
-              vm.checklistIdBeingEdited
-                ? editChecklist(vm.checklistIdBeingEdited)
-                : addChecklist()
-            "
+            (save)="addChecklist()"
           ></app-form-modal>
         </ng-template>
       </ion-modal>
@@ -62,8 +48,6 @@ export class HomeComponent {
   checklist$ = this.checklistService.getChecklists();
 
   formModalIsOpen$ = new BehaviorSubject<boolean>(false);
-
-  checklistIdBeingEdited$ = new BehaviorSubject<string | null>(null);
 
   checklistForm = this.fb.nonNullable.group({
     title: ['', Validators.required],
@@ -80,18 +64,6 @@ export class HomeComponent {
 
   deleteChecklist(id: string) {
     this.checklistService.remove(id);
-  }
-
-  editChecklist(checklistId: string) {
-    this.checklistService.update(checklistId, this.checklistForm.getRawValue());
-  }
-
-  openEditModal(checklist: Checklist) {
-    this.checklistForm.patchValue({
-      title: checklist.title,
-    });
-    this.checklistIdBeingEdited$.next(checklist.id);
-    this.formModalIsOpen$.next(true);
   }
 }
 
